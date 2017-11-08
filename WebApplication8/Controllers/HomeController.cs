c79286da6040e0409ed85fc833d5197d3c990ff1
15ngburton <15ngburton@gmail.com>
2017-11-08 16:23:12 -0700

    Add files via upload

--- WebApplication8/Controllers/HomeController.cs
+++ WebApplication8/Controllers/HomeController.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using static WebApplication8.Models.Mission;
 
 namespace WebApplication8.Controllers
 {
+    
     public class HomeController : Controller
     {
+        static List<Missions> missionList = new List<Missions>();
         public ActionResult Index()
         {
+            missionList.Add(new Missions() { id = 1, missionName = "England London Mission", missionPresidentName = "Mark Stevenson", missionAddress = "380 S. Piccadily Way, London, UK 483703", missionLanguage = "English", missionClimate = "Cold", dominantReligion = "Church of England", imgFileName = "" });
+
             return View();
         }
 
