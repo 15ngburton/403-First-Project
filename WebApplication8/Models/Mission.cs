38793992052c0b3e31472c76fc1f396a86e0116b
15ngburton <15ngburton@gmail.com>
2017-11-08 16:26:56 -0700

    Second Commit

---
+++ WebApplication8/Models/Mission.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication8.Models
+{
+    public class Mission
+    {
+
+        [Table("Mission")]
+        public class Missions
+        {
+            
+            [Key]
+            public int id { get; set; }
+            public string missionName { get; set; }
+            public string missionPresidentName { get; set; }
+            public string missionAddress { get; set; }
+            public string missionLanguage { get; set; }
+            public string missionClimate { get; set; }
+            public string dominantReligion { get; set; }
+            public string imgFileName { get; set; }
+        
+        }
+    }
+}
